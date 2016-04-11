9ceb1f0f84120f63898f608be1f50b292ed59afe
voidnologo <void.no.logo@icloud.com>
2016-04-11 11:48:02 -0500

    updates

--- aliases.sh
+++ aliases.sh
@@ -8,6 +8,6 @@ alias killhidden="defaults write com.apple.finder AppleShowAllFiles FALSE;killal
 alias pyclean="find . -name '*.pyc' -delete"
 alias start-redis="sudo redis-server /usr/local/etc/redis.conf"
 alias showstash="git stash list | awk -F: '{ print \"\n\n\n\n\"; print $0; print \"\n\n\"; system(\"git stash show -p \" $1); }'"
-alias git="hub"
+# alias git="hub"
 alias git-track='function _gittrack(){ git branch --set-upstream-to=origin/"$1" "$1";};_gittrack'
 alias which-tmux='tmux display-message -p "#S"'
--- custom-hub-completions.sh
+++
@@ -1,4 +0,0 @@
-# hub completions
-fpath=(~/dotfiles/custom-configs/zsh/completions $fpath)
-compinit
-compdef _hub git
