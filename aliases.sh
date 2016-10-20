4e68d747ed6474706eb94b5961275aeceffe8e5d
voidnologo <(none)>
2016-10-20 14:04:18 -0500

    mkvirtualenv alias

--- aliases.sh
+++ aliases.sh
@@ -13,3 +13,5 @@ alias git-track='function _gittrack(){ git branch --set-upstream-to=origin/"$1" 
 alias which-tmux='tmux display-message -p "#S"'
 alias ec2="AWS_DEFAULT_PROFILE=prod-eb-cli /Users/imtapps/.virtualenvs/aws-fuzzy/bin/aws-fuzzy --key-path ~/.ssh/staging-key-bastion.pem --tunnel --tunnel-key-path 'staging-key-beanstalk.pem'"
 alias ec2-prod="AWS_DEFAULT_PROFILE=prod-eb-cli /Users/imtapps/.virtualenvs/aws-fuzzy/bin/aws-fuzzy --key-path ~/.ssh/production-key-bastion.pem --tunnel --tunnel-key-path 'production-key-beanstalk.pem'"
+alias mkvenv="mkvirtualenv $1"
+alias mkvenv3="mkvirtualenv $1 -ppython3.4"
